421e665b4756916d3639cfc7261cd9662d58f385
Plenegeek <pietertschopp065@gmail.com>
2026-08-24 20:20:42 -0700

    Fix syntax errors and update file paths in cpu.cpp

--- cpu.cpp
+++ cpu.cpp
@@ -3,16 +3,17 @@
 
 
 double cpu_usage() {
-	return 42.0 // Placeholder. Need to work on getting usage correctly
+        return 42.0; // Placeholder. Need to work on getting usage correctly
 }
 double cpu_temp() {
 
 
-	std::ifstream file("/sys/class/termal_zone0/temp"); // Gets the temp for the CPU WARNING!!!!: ONLY FOR LINUX!!!!
+        std::ifstream file("/sys/class/thermal/thermal_zone0/temp"); // Gets the temp for the CPU WARNING!!!!: ON>
 
-	int temp;
+        int temp;
+        file >> temp;
 
-	return temp / 1000.0;  // The Temp in the file is showed at  micro or milli C. This converts it to normal C
+        return temp / 1000.0;  // The Temp in the file is showed at  micro or milli C. This converts it to normal>
 
 }
 // End program
