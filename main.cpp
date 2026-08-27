75cbd2ee0554b4924ef8eab534bd6a910c3be9f9
Plenegeek <pietertschopp065@gmail.com>
2026-08-26 19:34:14 -0700

    Fix formatting in main.cpp output

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
   std::cout << "----------------------------------------\n";
   std::cout << "FTMS -- Fast Terminal Monitoring System\n";
   std::cout << "----------------------------------------\n";
-  std::cout << "\n"
+  std::cout << "\n";
   std::cout << "CPU Usage:      " << cpu_usage() << "%\n";
   std::cout << "CPU Temp:       " << cpu_temp() << "C\n";
   std::cout << "----------------------------------------\n";
