6057d399d15bbcec64470984617763d671d69e00
Plenegeek <pietertschopp065@gmail.com>
2026-08-27 07:52:29 -0700

    Include RAM usage in FTMS output
    Added RAM usage display to the FTMS output.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include "cpu.hpp" 
+#include "memory.hpp"
 // Made by Pieter Tschopp
 // FTMS - Fast Terminal Monitoring System is a free-open-source program to moniter your hardware without covering your terminal(I.E. btop, htop, ect.)
-// Modify and change at will. 
+// Modify and change at will.
 // For any bugs, questions, or suggestions: Contact pietertschopp065@gmail.com.
-// Thanks! And have a good day
-
+// Thanks! And have a good day!
 
 int main() {
 
   std::cout << "----------------------------------------\n";
   std::cout << "FTMS -- Fast Terminal Monitoring System\n";
   std::cout << "----------------------------------------\n";
-  std::cout << "\n";
   std::cout << "CPU Usage:      " << cpu_usage() << "%\n";
   std::cout << "CPU Temp:       " << cpu_temp() << "C\n";
+  std::cout << "RAM:            " << memory_total() << "\n"; 
   std::cout << "----------------------------------------\n";
         return 0;
 
 }
+
