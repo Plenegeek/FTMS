162747b61d3300ad3a6969a94dd571fc02d19651
Plenegeek <pietertschopp065@gmail.com>
2026-08-27 19:47:02 -0700

    Modify RAM output format and include free RAM
    Updated RAM output to display in GB and added free RAM information.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include "cpu.hpp" 
+#include "cpu.hpp"
 #include "memory.hpp"
 // Made by Pieter Tschopp
 // FTMS - Fast Terminal Monitoring System is a free-open-source program to moniter your hardware without covering your terminal(I.E. btop, htop, ect.)
 // Modify and change at will.
 // For any bugs, questions, or suggestions: Contact pietertschopp065@gmail.com.
 // Thanks! And have a good day!
-
 int main() {
 
   std::cout << "----------------------------------------\n";
@@ -14,9 +13,9 @@ int main() {
   std::cout << "----------------------------------------\n";
   std::cout << "CPU Usage:      " << cpu_usage() << "%\n";
   std::cout << "CPU Temp:       " << cpu_temp() << "C\n";
-  std::cout << "RAM:            " << memory_total() << "\n"; 
+  std::cout << "RAM:            " << memory_total() << " GB\n";
+  std::cout << "RAM free:      "  << memory_free() << " GB\n";
   std::cout << "----------------------------------------\n";
         return 0;
 
 }
-
