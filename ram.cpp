43543bc039ae5d7915d1a29a79cec0cb25e49cea
Plenegeek <pietertschopp065@gmail.com>
2026-08-27 19:47:30 -0700

    Implement memory_free function to check available memory
    Add function to retrieve free memory in GB from /proc/meminfo

--- ram.cpp
+++ ram.cpp
@@ -16,3 +16,18 @@ double memory_total() {
 
     return 0;
 }
+
+double memory_free() {
+    std::ifstream file("/proc/meminfo");
+
+    std::string name;
+    double value;
+
+    while (file >> name >> value) {
+        if (name == "MemAvailable:") {
+            return value / 1024.0 / 1024.0;
+        }
+    }
+
+    return 0;
+}
