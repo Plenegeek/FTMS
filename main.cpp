1a082267acdf8ccc3fa681535d3e320dc31ed655
Plenegeek <pietertschopp065@gmail.com>
2026-08-28 21:57:56 -0700

    Add uptime display to main output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "cpu.hpp"
 #include "memory.hpp"
+#include "uptime.hpp"
 // Made by Pieter Tschopp
 // FTMS - Fast Terminal Monitoring System is a free-open-source program to moniter your hardware without covering your terminal(I.E. btop, htop, ect.)
 // Modify and change at will.
@@ -15,10 +16,8 @@ int main() {
   std::cout << "CPU Temp:       " << cpu_temp() << "C\n";
   std::cout << "RAM:            " << memory_total() << " GB\n";
   std::cout << "RAM free:       "  << memory_free() << " GB\n";
+  std::cout << "Uptime:         " << get_uptime() << "s\n"; 
   std::cout << "----------------------------------------\n";
         return 0;
 
 }
-
-
-
