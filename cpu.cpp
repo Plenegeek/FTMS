6cb42251fe3432d8e60966a0bb1ac3caa16fd57f
Plenegeek <pietertschopp065@gmail.com>
2026-08-28 22:51:55 -0700

    Fix comment formatting in cpu_temp function

--- cpu.cpp
+++ cpu.cpp
@@ -33,14 +33,13 @@ double cpu_usage() {
 double cpu_temp() {
 
 
-        std::ifstream file("/sys/class/thermal/thermal_zone0/temp"); // Gets the temp for the CPU WARNING!!!!: ON>
+        std::ifstream file("/sys/class/thermal/thermal_zone0/temp"); // Gets the temp for the CPU
 
         int temp;
         file >> temp;
 
-        return temp / 1000.0;  // The Temp in the file is showed at  micro or milli C. This converts it to normal>
+        return temp / 1000.0;  // The Temp in the file is showed at  micro or milli C. This converts it to normal C Temp
 
 }
-// End program
 
 
