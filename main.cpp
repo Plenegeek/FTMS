21289ef49621a97252e40696f6f9304e4d3af4e2
Plenegeek <pietertschopp065@gmail.com>
2026-09-03 21:46:30 -0700

    Enhance uptime output formatting
    Refactor uptime display to show days, hours, and minutes.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,33 @@
 #include "cpu.hpp"
 #include "memory.hpp"
 #include "uptime.hpp"
+#include <iomanip>
 // Made by Pieter Tschopp
 // FTMS - Fast Terminal Monitoring System is a free-open-source program to moniter your hardware without covering your terminal(I.E. btop, htop, ect.)
 // Modify and change at will.
 // For any bugs, questions, or suggestions: Contact pietertschopp065@gmail.com.
 // Thanks! And have a good day!
 int main() {
-
-  std::cout << "----------------------------------------\n";
+    std::cout << std::fixed << std::setprecision(2);
+  std::cout << "-----------------------------------------\n";
   std::cout << "FTMS -- Fast Terminal Monitoring System\n";
-  std::cout << "----------------------------------------\n";
+  std::cout << "-----------------------------------------\n";
   std::cout << "CPU Usage:      " << cpu_usage() << "%\n";
   std::cout << "CPU Temp:       " << cpu_temp() << "C\n";
   std::cout << "RAM:            " << memory_total() << " GB\n";
   std::cout << "RAM free:       "  << memory_free() << " GB\n";
-  std::cout << "Uptime:         " << get_uptime() << "s\n"; 
-  std::cout << "----------------------------------------\n";
-        return 0;
+    double uptime = get_uptime();
 
+    int days = static_cast<int>(uptime / 86400);
+    int hours = static_cast<int>((uptime / 3600)) % 24;
+    int minutes = static_cast<int>((uptime / 60)) % 60;
+
+    std::cout << "Uptime:         "
+              << days << " Day(s) "
+              << hours << " Hour(s) "
+              << minutes << " Minute(s)\n";
+
+    std::cout << "-----------------------------------------\n";
+
+        return 0;
 }
